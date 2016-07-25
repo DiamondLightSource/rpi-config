29b78ba7285b890e44b58ca1ee1d9d6eb925b9a9
Ben Seeley <ben.seeley@diamond.ac.uk>
2016-07-25 17:14:11 +0100

    Server remains open on disconnect

--- rpi-gpio-server/rpiGpioServer.py
+++ rpi-gpio-server/rpiGpioServer.py
@@ -29,12 +29,13 @@ if s is None:
     print ("could not open socket") 
     sys.exit(1)
 
-conn, addr = s.accept() 
-print ("Connected by", addr) 
-while 1:
-    data = conn.recv(1024) 
-    if not data: 
-        break 
-    conn.send(data)
-
-conn.close()
+while True:
+    conn, addr = s.accept() 
+    print ("Connected by", addr) 
+    while 1:
+        data = conn.recv(1024) 
+        if not data: 
+            break 
+        conn.send(data)
+    
+    conn.close()
