f59cb952995fbb36e0f5093620628cd0e0fd24b1
Ben Seeley <benjamin.seeley97@gmail.com>
2016-08-09 16:35:00 +0100

    Reformatted Arduino Script to improve legibility

--- arduino/i2cSlaveTest/i2cSlaveTest.ino
+++ arduino/i2cSlaveTest/i2cSlaveTest.ino
@@ -5,45 +5,43 @@ int number = 0;
 int state = 0;
 
 void setup() {
-pinMode(9, OUTPUT);
-Serial.begin(9600); // start serial for output
-// initialize i2c as slave
-Wire.begin(SLAVE_ADDRESS);
+  pinMode(9, OUTPUT);
+  Serial.begin(9600); // start serial for output
+  // initialize i2c as slave
+  Wire.begin(SLAVE_ADDRESS);
 
-// define callbacks for i2c communication
-Wire.onReceive(receiveData);
-Wire.onRequest(sendData);
+  // define callbacks for i2c communication
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
 
-Serial.println("Ready!");
+  Serial.println("Ready!");
 }
 
 void loop() {
-delay(100);
+  delay(100);
 }
 
 // callback for received data
 void receiveData(int byteCount){
+  while(Wire.available()) {
+    number = Wire.read();
+    Serial.print("data received: ");
+    Serial.println(number);
 
-while(Wire.available()) {
-number = Wire.read();
-Serial.print("data received: ");
-Serial.println(number);
-
-if (number == 1){
-
-if (state == 0){
-digitalWrite(9, HIGH); // set the LED on
-state = 1;
-}
-else{
-digitalWrite(9, LOW); // set the LED off
-state = 0;
-}
-}
-}
+    if (number == 1){
+      if (state == 0){
+        digitalWrite(9, HIGH); // set the LED on
+        state = 1;
+      }else{
+        digitalWrite(9, LOW); // set the LED off
+        state = 0;
+      }
+    }
+  }
 }
 
 // callback for sending data
 void sendData(){
-Wire.write(number);
+  Wire.write(number);
 }
+
