ee7de1808a5d71b39f80826e863ef8946a4b399e
Ben Seeley <benjamin.seeley97@gmail.com>
2016-08-10 14:16:24 +0100

    Outlines needed development in arduino client

--- arduino/i2cArduinoClient/i2cArduinoClient.ino
+++ arduino/i2cArduinoClient/i2cArduinoClient.ino
@@ -2,6 +2,7 @@
 
 #define SLAVE_ADDRESS 0x04
 
+
 void setup() {
   // initialize i2c as slave
   Wire.begin(SLAVE_ADDRESS);
@@ -25,5 +26,22 @@ void receiveData(int byteCount){
 // callback for sending data
 void sendData(){
   Wire.write();
+}
+
+void parseData(){
+  //Parses input buffer 
+  /*
+   * Needs the following command options:
+   * pinMode
+   * analogReference
+   * digitalWrite
+   * digitalRead
+   * analogRead
+   * analogWrite (this is what pwm is called)
+   * 
+   * Will also need a control framework;
+   * e.g. prior to a data request, a command will be recieved to take a reading or whatever, that data should be queued to a buffer 
+   * in anticipation of a read request to offload everything.
+   */
 }
 
