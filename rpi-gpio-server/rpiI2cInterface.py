d4b47cb02e4ea7721e12fd2d0f508cb1e6e7e2be
Ben Seeley <ben.seeley@diamond.ac.uk>
2016-08-10 09:42:26 +0100

    Adds I2C device creation

--- rpi-gpio-server/rpiI2cInterface.py
+++ rpi-gpio-server/rpiI2cInterface.py
@@ -5,6 +5,25 @@ import com.pi4j.io.i2c.I2CDevice as I2CDevice
 import com.pi4j.io.i2c.I2CFactory as I2CFactory
 
 class Interface():
+    interfaceDevices = []
     def __init__(self):
         self.bus = I2CFactory.getInstance(I2CBus.BUS_1)
+    
+    def createDevice(self, name, busAddress):
+        device = self.bus.getDevice(busAddress)
+        device.setName(name) 
+        Interface.interfaceDevices.append(device)
         
+    def write(self, deviceName, message):
+        """
+        Need to set up some sort of buffer to offload multibyte messages to the bus, might work with native bytearray type?
+        """
+        for device in Interface.interfaceDevices:
+            if deviceName == device.getName():
+                device.write(message)             
+                break
+            else:
+                pass
+            
+
+           
