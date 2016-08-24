d0dbd718bc4f49d35a63826c62e575db2d9fb299
Ben Seeley <ben.seeley@diamond.ac.uk>
2016-08-24 17:19:01 +0100

    Camera Init

--- rpi-hardware-server/rpiCamera.py
+++ rpi-hardware-server/rpiCamera.py
@@ -1,7 +1,25 @@
 import com.hopding.jrpicam as RPiCam
-import time
+from datetime import date
 
 class Camera:
     def __init__(self):
-        self.dirStub = "/home/pi/"
-        self.cam = RPiCam.RPiCamera()
+        self.date = date.today()
+        self.dirStub = "/home/pi/gda_data_non_live"
+        self.fileCount = 0
+        dirCheck()
+        if fileCount == -1:
+            fileFind()
+        self.cam = RPiCam.RPiCamera(self.fullDir)
+        
+        
+                
+    def dirCheck(self):
+        self.fullDir = dirStub +"/"+ str(self.date.year) +"/"+ str(self.date.month) +"/"+ str(self.date.day)
+        if not os.path.exists(self.fullDir):
+            os.makedirs(self.fullDir)
+        else:
+            self.fileCount = -1
+            
+    def fileFind(self):
+        ##check for files and find next clear Value
+        
