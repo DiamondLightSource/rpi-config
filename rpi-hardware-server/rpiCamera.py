d5baa6249cee066116368f29ed4b5fccebfd7526
Ben Seeley <ben.seeley@diamond.ac.uk>
2016-08-30 18:02:03 +0100

    INT

--- rpi-hardware-server/rpiCamera.py
+++ rpi-hardware-server/rpiCamera.py
@@ -10,7 +10,7 @@ class Camera:
         self.fileName = self.defaultFileName
         self.fileCount = 0
         self.cam = RPiCam.RPiCamera()
-        self.cam.setTimeout(0.1)
+        self.cam.setTimeout(1)  #must be int
         
     def parse(self, num, instr, command, pathString, duration):
         print command
